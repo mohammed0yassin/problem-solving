f6ea0761e1b0ba5083eed4410814a2b3c4f80765
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2023-06-25 17:51:35 +0300

    Modify: 46. Permutations

--- 46. Permutations/Solution46.java
+++ 46. Permutations/Solution46.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution46 {
-    public List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         backtrack(new ArrayList<>(), ans, nums);
-        return null;
+        return ans;
     }
 
-    public void backtrack(List<Integer> current, List<List<Integer>> ans, int[] nums) {
+    public static void backtrack(List<Integer> current, List<List<Integer>> ans, int[] nums) {
         if (current.size() == nums.length) {
             ans.add(new ArrayList<>(current));
             return;
@@ -24,5 +24,7 @@ class Solution46 {
     }
 
     public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(permute(nums));
     }
 }
