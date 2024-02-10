fa33a6a6f731606263a0e8194dada6e10224196a
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2024-02-10 17:05:28 +0200

    Faster Solution: 125. Valid Palindrome

--- 125. Valid Palindrome/Solution125.java
+++ 125. Valid Palindrome/Solution125.java
@@ -1,14 +1,38 @@
 class Solution125 {
+//    public static boolean isPalindrome(String s) {
+//        if (s.length() <= 1) {
+//            return true;
+//        }
+//        s = s.toLowerCase();
+//        s = s.replaceAll("[^\\w+]|_", "");
+//        for (int i=0; i < s.length()/2; i++) {
+//            if (s.charAt(i) != s.charAt(s.length() -1 -i)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
     public static boolean isPalindrome(String s) {
         if (s.length() <= 1) {
             return true;
         }
-        s = s.toLowerCase();
-        s = s.replaceAll("[^\\w+]|_", "");
-        for (int i=0; i < s.length()/2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() -1 -i)) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+            left++;
+            right--;
         }
         return true;
     }
