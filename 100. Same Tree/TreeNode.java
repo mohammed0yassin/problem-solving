f419f5bbcf1854fb8e070f62968adc4e47f412ea
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2024-05-28 00:18:41 +0300

    Add: Tree from compact array

--- 100. Same Tree/TreeNode.java
+++ 100. Same Tree/TreeNode.java
@@ -19,16 +19,46 @@ public class TreeNode {
     }
 
     TreeNode(Integer[] treeArray) {
-        this.val = treeArray[0];
-        buildTreeFromArray(this, treeArray, 0);
+        this(treeArray, "compact");
     }
 
+    TreeNode(Integer[] treeArray, String style) {
+        if (style.equals("expanded")) {
+            this.val = treeArray[0];
+            buildTreeFromArray(this, treeArray, 0);
+        } else {
+            this.val = treeArray[0];
+            buildTreeFromCompactArray(this, treeArray);
+        }
+    }
+
+    private void buildTreeFromCompactArray(TreeNode treeNode, Integer[] treeArray) {
+        if (treeArray[0] == null) return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(treeNode);
+        int idx = 0;
+        while (idx < treeArray.length) {
+            TreeNode current = queue.poll();
+            if (idx + 1 < treeArray.length && treeArray[idx + 1] != null) {
+                current.left = new TreeNode(treeArray[idx + 1]);
+                queue.add(current.left);
+            }
+
+            if (idx + 2 < treeArray.length && treeArray[idx + 2] != null) {
+                current.right = new TreeNode(treeArray[idx + 2]);
+                queue.add(current.right);
+            }
+            idx += 2;
+        }
+    }
+
+
     private void buildTreeFromArray(TreeNode root, Integer[] children, Integer idx) {
-        if (root == null || 2*idx + 1 >= children.length) return;
-        if (children[2*idx +1] != null) root.right = new TreeNode(children[2*idx+1]);
-        if (children[2*idx+2] != null) root.left = new TreeNode(children[2*idx+2]);
-        buildTreeFromArray(root.right, children, 2*idx+1);
-        buildTreeFromArray(root.left, children, 2*idx+2);
+        if (root == null || 2 * idx + 1 >= children.length) return;
+        if (children[2 * idx + 1] != null) root.right = new TreeNode(children[2 * idx + 1]);
+        if (children[2 * idx + 2] != null) root.left = new TreeNode(children[2 * idx + 2]);
+        buildTreeFromArray(root.right, children, 2 * idx + 1);
+        buildTreeFromArray(root.left, children, 2 * idx + 2);
     }
 
     static ArrayList<Integer> allTree = new ArrayList<>();
@@ -70,24 +100,24 @@ public class TreeNode {
 
 
     public static void main(String[] args) {
-        TreeNode ten        = new TreeNode(10);
-        TreeNode eleven     = new TreeNode(11);
-        TreeNode fiften     = new TreeNode(15);
-        TreeNode sixteen    = new TreeNode(16);
-        TreeNode twenty     = new TreeNode(20);
-        TreeNode twentyOne  = new TreeNode(21);
+        TreeNode ten = new TreeNode(10);
+        TreeNode eleven = new TreeNode(11);
+        TreeNode fiften = new TreeNode(15);
+        TreeNode sixteen = new TreeNode(16);
+        TreeNode twenty = new TreeNode(20);
+        TreeNode twentyOne = new TreeNode(21);
         TreeNode twentyFive = new TreeNode(25);
-        TreeNode TwentySix  = new TreeNode(26);
-        TreeNode one        = new TreeNode(1, ten, eleven);
-        TreeNode three      = new TreeNode(3, fiften, sixteen);
-        TreeNode two        = new TreeNode(2, one, three);
-        TreeNode six        = new TreeNode(6, twenty, twentyOne);
-        TreeNode nine       = new TreeNode(9, twentyFive, TwentySix);
-        TreeNode seven      = new TreeNode(7, six, nine);
-        TreeNode root       = new TreeNode(4, two, seven);
+        TreeNode TwentySix = new TreeNode(26);
+        TreeNode one = new TreeNode(1, ten, eleven);
+        TreeNode three = new TreeNode(3, fiften, sixteen);
+        TreeNode two = new TreeNode(2, one, three);
+        TreeNode six = new TreeNode(6, twenty, twentyOne);
+        TreeNode nine = new TreeNode(9, twentyFive, TwentySix);
+        TreeNode seven = new TreeNode(7, six, nine);
+        TreeNode root = new TreeNode(4, two, seven);
 //        printAllTree(root);
-        Integer[] treeArray = {1, null, 0, null, 1, 0, 1, 1, null, null, 0, null, null, null, 1, 0, null, null, 0, null, null, 1};
-        TreeNode treeRoot = new TreeNode(treeArray);
-        System.out.println(traverseTreeToArrayWithNulls(treeRoot, 0));
+        Integer[] treeArrayCompact = {1, null, 0, null, 1, 0, 1, 1, null, null, 0, null, null, null, 1, 0};
+        TreeNode treeRootCompact = new TreeNode(treeArrayCompact);
+        System.out.println(traverseTreeToArrayWithNulls(treeRootCompact, 0));
     }
 }
